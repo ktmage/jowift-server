52dd4efb727ae5531115e5b3edf74ab09256a002
natsuki <yasashisainternet@gmail.com>
2023-10-22 19:25:08 +0900

    database!!!!!!

--- routes/index.ts
+++ routes/index.ts
@@ -1,47 +1,26 @@
 import express from 'express';
+import pool from '../database/pool';
 const router = express.Router();
-// const pool = require("../database/pool");
 
-// GET: /notes
-// Returns all notes in the database.
-// router.get("/notes", async (req, res) => {
-//   const connection = await pool.getConnection();
-//   try {
-//     const query =
-//     "SELECT notes.id, notes.title, notes.body, GROUP_CONCAT(tags.name SEPARATOR ',') AS tags FROM notes JOIN note_tag ON notes.id = note_tag.note_id JOIN tags ON tags.id = note_tag.tag_id GROUP BY notes.id";
+router.get("/notes", async (req, res) => {
+  const connection = await pool.getConnection();
+  try {
+    const query =
+    "SELECT notes.id, notes.title, notes.body, GROUP_CONCAT(tags.name SEPARATOR ',') AS tags FROM notes JOIN note_tag ON notes.id = note_tag.note_id JOIN tags ON tags.id = note_tag.tag_id GROUP BY notes.id";
 
-//     const rows = await connection.query(query);
-//     res.header("Content-Type", "application/json; charset=utf-8");
-//     res.status(200).json(rows[0]);
-//   } catch (error) {
-//     console.log(error);
-//   } finally {
-//     connection.release();
-//   }
-// });
-
-// router.post("/notes", async (req, res) => {
-//   console.log(req.body);
-//   res.status(200).json("OK");
-//   // res.status(400).json("NO"); // ex: this is error response.
-// });
-
-// ルーティング
-router.get('/hello', (req: express.Request, res: express.Response) => {
-	res.send('Hello World');
+    const rows = await connection.query(query);
+    res.header("Content-Type", "application/json; charset=utf-8");
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    connection.release();
+  }
 });
 
-router.get('/hello/:name', (req: express.Request, res: express.Response) => {
-	res.send(`Hello ${req.params.name}`);
-});
-
-// さようなら
-router.get('/goodbye', (req: express.Request, res: express.Response) => {
-	res.send('Goodbye');
-});
-
-router.get('/goodbye/:name', (req: express.Request, res: express.Response) => {
-	res.send(`Goodbye ${req.params.name}`);
+router.post("/notes", async (req, res) => {
+  console.log(req.body);
+  res.status(200).json("OK");
 });
 
 export default router;

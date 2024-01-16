58f1e1424567e93b51c31d855e49a8ed16a75664
natsuki <yasashisainternet@gmail.com>
2024-01-16 22:59:33 +0900

    行進しました。・

--- src/server.ts
+++ src/server.ts
@@ -69,14 +69,16 @@ app.use(express.json());
 // sessionの設定
 app.use(session(sessionOptions));
 
+console.log(__dirname, '../public');
+
 // 静的ファイルの設定
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(path.resolve(__dirname, '../public'))));
 
 // ルーティングの設定
 app.use('/api', Router);
 
 app.get('*', (req, res) => {
-	res.sendFile(__dirname + '/public/index.html');
+	res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
 // サーバーの起動

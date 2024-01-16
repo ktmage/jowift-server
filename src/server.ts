b0f2d6f56fad9d0d60aab3bd63cd74356209a37f
natsuki <yasashisainternet@gmail.com>
2024-01-16 22:35:03 +0900

    静的ファイルを配信するように変更

--- src/server.ts
+++ src/server.ts
@@ -3,38 +3,39 @@ import session from 'express-session';
 import Router from './routes';
 import { PrismaClient } from '@prisma/client';
 import { getEnvVariable } from './utility';
-import cors from 'cors';
+// import cors from 'cors';
 
 // prismaでセッションを管理するためのミドルウェア
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import path from 'path';
 
 // 環境変数の読み込み
 const PORT = Number(getEnvVariable('PORT'));
 const SESSION_LIMIT_DAYS = Number(getEnvVariable('SESSION_LIMIT_DAYS'));
 const SESSION_SECRET = getEnvVariable('SESSION_SECRET');
 const SESSION_CHECK_PERIOD_MINUTES = Number(getEnvVariable('SESSION_CHECK_PERIOD_MINUTES'));
-const ALLOW_ORIGIN = getEnvVariable('ALLOW_ORIGIN');
-const ALLOW_METHODS = getEnvVariable('ALLOW_METHODS');
-const ALLOW_HEADERS = getEnvVariable('ALLOW_HEADERS');
-const SAME_SITE = <boolean | 'lax' | 'strict' | 'none'>getEnvVariable('SAME_SITE');
-const SECURE = Boolean(getEnvVariable('SECURE'));
+// const ALLOW_ORIGIN = getEnvVariable('ALLOW_ORIGIN');
+// const ALLOW_METHODS = getEnvVariable('ALLOW_METHODS');
+// const ALLOW_HEADERS = getEnvVariable('ALLOW_HEADERS');
+// const SAME_SITE = <boolean | 'lax' | 'strict' | 'none'>getEnvVariable('SAME_SITE');
+// const SECURE = Boolean(getEnvVariable('SECURE'));
 const HTTP_ONLY = Boolean(getEnvVariable('HTTP_ONLY'));
-const DOMAIN = getEnvVariable('DOMAIN');
+// const DOMAIN = getEnvVariable('DOMAIN');
 
 
-const corsOptions: cors.CorsOptions = {
-	origin: ALLOW_ORIGIN,
-	methods: ALLOW_METHODS,
-	allowedHeaders: ALLOW_HEADERS,
-	credentials: true,
-}
+// const corsOptions: cors.CorsOptions = {
+// 	origin: ALLOW_ORIGIN,
+// 	methods: ALLOW_METHODS,
+// 	allowedHeaders: ALLOW_HEADERS,
+// 	credentials: true,
+// }
 
 const cookieOptions: express.CookieOptions = {
 	maxAge: SESSION_LIMIT_DAYS * 24 * 60 * 60 * 1000,
 	// sameSite: SAME_SITE,
 	// secure: SECURE,
 	httpOnly: HTTP_ONLY,
-	domain: DOMAIN,
+	// domain: DOMAIN,
 }
 
 const sessionOptions: session.SessionOptions = {
@@ -56,25 +57,27 @@ declare module 'express-session' {
 	}
 }
 
-console.log("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-console.log(corsOptions);
-console.log(cookieOptions);
-console.log("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-
+// expressの設定
 const app: express.Express = express();
-
 
 // jsonを読むための設定
 app.use(express.json());
 
 // corsの設定
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // sessionの設定
 app.use(session(sessionOptions));
 
+// 静的ファイルの設定
+app.use(express.static(path.join(__dirname, 'public')));
+
 // ルーティングの設定
-app.use('/', Router);
+app.use('/api', Router);
+
+app.get('*', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
 
 // サーバーの起動
 app.listen(PORT, () => {

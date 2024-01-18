6785c2893fa77b9d4d8c43fe988a1721c287b4f1
natsuki <yasashisainternet@gmail.com>
2024-01-19 01:53:11 +0900

    環境変数とdotenv周りを整備

--- src/utility/getEnvVariable.ts
+++ src/utility/getEnvVariable.ts
@@ -1,4 +1,12 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import path from 'path';
+
+// 環境変数の読み込み
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+// .envファイルの読み込み
+dotenv.config({ path: path.resolve(process.cwd(), `.env.${nodeEnv}.local`) });
+
 export default function getEnvVariable(key: string) {
 	const value = process.env[key];
 	if (!value) {

e23c3249b2df98514e207bdbdf3ee19656eb0e5d
natsuki <yasashisainternet@gmail.com>
2024-01-20 04:59:32 +0900

    [add] src/config/index.tsの追加
    環境変数を読み込むためのファイルを追加しました。

---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+import { getEnvVariable } from '../utility';
+
+export const CORS = {
+	ALLOW_ORIGIN: getEnvVariable('CORS_ALLOW_ORIGIN'),
+	ALLOW_METHODS: getEnvVariable('CORS_ALLOW_METHODS'),
+	ALLOW_HEADERS: getEnvVariable('CORS_ALLOW_HEADERS'),
+};
+
+export const COOKIE = {
+	SAME_SITE: <boolean | 'lax' | 'strict' | 'none'>getEnvVariable('COOKIE_SAME_SITE'),
+	SECURE: getEnvVariable('COOKIE_SECURE') === 'true',
+	HTTP_ONLY: getEnvVariable('COOKIE_HTTP_ONLY') === 'true',
+	DOMAIN: getEnvVariable('COOKIE_DOMAIN'),
+};
+
+export const SESSION = {
+	SECRET: getEnvVariable('SESSION_SECRET'),
+	LIMIT_DAYS: Number(getEnvVariable('SESSION_LIMIT_DAYS')),
+	CHECK_PERIOD_MINUTES: Number(getEnvVariable('SESSION_CHECK_PERIOD_MINUTES')),
+};
+
+export const PORT = Number(getEnvVariable('PORT'));

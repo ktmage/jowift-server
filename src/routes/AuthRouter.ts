430fadf20d6d5350654ce153e47509914c63b8c2
natsuki <yasashisainternet@gmail.com>
2024-02-01 23:23:40 +0900

    [update] AuthRouter.tsにGoogle認証用のルーティングを追加

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import authController from '../controller/AuthController';
+import passport from 'passport';
 
 const router = express.Router();
 
@@ -9,5 +10,16 @@ router.post('/signup', authController.SignUp);
 router.get('/logout', authController.Logout);
 router.get('/user', authController.getUser);
 router.delete('/user', authController.deleteUser);
+
+// Google OAuth
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get(
+	'/google/callback',
+	passport.authenticate('google', {
+		successRedirect: 'http://localhost:3000/',
+		failureRedirect: 'http://localhost:3000/',
+		session: true,
+	}),
+);
 
 export default router;

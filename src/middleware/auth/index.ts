7968d1ae2269423c9fde86ed528f4699a31f741a
natsuki <yasashisainternet@gmail.com>
2024-02-01 23:22:33 +0900

    [add] passportを使った認証のミドルウェアを追加

---
+++ src/middleware/auth/index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import passport from 'passport';
+import googleStrategy from './googleStrategy';
+import express from 'express';
+
+const prisma = new PrismaClient();
+
+const router = express.Router();
+
+//
+passport.use(googleStrategy);
+
+passport.serializeUser((userId, done) => {
+	console.log('>> serializeUser');
+	done(null, userId);
+});
+
+passport.deserializeUser(async (userId, done) => {
+	console.log('>> deserializeUser');
+	const user = await prisma.user.findUnique({ where: { id: userId as string } });
+	if (user) {
+		done(null, {
+			id: user.id.toString(),
+			name: user.name,
+			email: user.email,
+			photo: user.photoUrl,
+		});
+	} else {
+		done(new Error('User not found'), false);
+	}
+});
+// passportの初期化
+router.use(passport.initialize());
+
+export default router;
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,1 +1,2 @@
 export { default as isAuthenticated } from './isAuthenticated';
+export { default as auth } from './auth';

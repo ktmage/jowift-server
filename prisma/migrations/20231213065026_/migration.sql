56abe10b36ed03c504ef800ade657b40eda29095
ktmage <yasashisainternet@gmail.com>
2024-03-30 22:35:17 +0900

    [update] : migration

--- prisma/migrations/20231213065026_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `notetag` DROP FOREIGN KEY `NoteTag_tagId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `NoteTag` ADD CONSTRAINT `NoteTag_tagId_fkey` FOREIGN KEY (`tagId`) REFERENCES `Tag`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20231214001826_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `note` MODIFY `content` TEXT NOT NULL;

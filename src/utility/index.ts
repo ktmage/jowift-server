aa96551cf4e34baa6b6570850cb44d4331d9808e
ktmage <yasashisainternet@gmail.com>
2024-06-03 21:44:02 +0900

    [add] : handleErrorを追加
    エラーハンドリングのためのミドルウェアを追加しました。

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,3 +1,4 @@
 export { default as getEnvVariable } from './getEnvVariable';
 export { default as ServiceError } from './ServiceError';
 export { default as RequestError } from './RequestError';
+export { default as handleError } from './handleError';

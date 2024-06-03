57ab0363d7fb305ec0626135fd031476c56a7bc5
ktmage <yasashisainternet@gmail.com>
2024-06-03 21:54:47 +0900

    [update] : AuthController.tsを修正
    エラーハンドリング周りを修正したり、スリムにしたり。

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,41 +1,39 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import AuthService from '../service/AuthService';
+import RequestError from '../utility/RequestError';
+import { UserModel } from '../models/UserModel';
 
 // 認証用コントローラー
 class AuthController {
-	static async SignUp(req: Request, res: Response) {
-		const { name, email, password } = req.body;
-		if (!name || !email || !password) {
-			res.status(400).send({ message: 'invalid request.' });
-			return;
-		}
-
-		const result = await AuthService.SignUp(name, email, password);
-		if (result.status) {
-			req.session.userId = result.data;
+	static async signUp(req: Request, res: Response, next: NextFunction) {
+		try {
+			const { name, email, password } = req.body;
+			if (!name || !email || !password) {
+				throw new RequestError('invalid request.');
+			}
+			const user = await AuthService.signUp(name, email, password);
+			req.session.userId = user.id;
 			res.status(200).send({ message: 'success.' });
-		} else {
-			res.status(400).send({ message: result.error });
+		} catch (e) {
+			next(e);
 		}
 	}
 
-	static async Login(req: Request, res: Response) {
-		const { email, password } = req.body;
-		if (!email || !password) {
-			res.status(400).send({ message: 'invalid request.' });
-			return;
-		}
-
-		const result = await AuthService.Login(email, password);
-		if (result.status) {
-			req.session.userId = result.data;
+	static async login(req: Request, res: Response, next: NextFunction) {
+		try {
+			const { email, password } = req.body;
+			if (!email || !password) {
+				throw new RequestError('invalid request.');
+			}
+			const result = await AuthService.login(email, password);
+			req.session.userId = result.id;
 			res.status(200).send({ message: 'success.' });
-		} else {
-			res.status(400).send({ message: result.error });
+		} catch (e) {
+			next(e);
 		}
 	}
 
-	static async Session(req: Request, res: Response) {
+	static async session(req: Request, res: Response) {
 		if (req.session.userId || req.user) {
 			res.status(200).send({ message: 'success.' });
 		} else {
@@ -43,7 +41,7 @@ class AuthController {
 		}
 	}
 
-	static async Logout(req: Request, res: Response) {
+	static async logout(req: Request, res: Response) {
 		req.session.destroy((err) => {
 			if (err) {
 				res.status(400).send({ message: 'failed.' });
@@ -53,35 +51,33 @@ class AuthController {
 		});
 	}
 
-	static async getUser(req: Request, res: Response) {
-		const userId = req.session.userId ?? req.user?.id;
+	static async getUser(req: Request, res: Response, next: NextFunction) {
+		try {
+			const userId = req.session.userId ?? req.user?.id;
 
-		if (!userId) {
-			res.status(401).send({ message: 'no active session.' });
-			return;
-		}
+			if (!userId) {
+				throw new RequestError('no active session.');
+			}
 
-		const result = await AuthService.getUser(userId);
-		if (result.status) {
-			res.status(200).send({ message: 'success.', data: result.data });
-		} else {
-			res.status(400).send({ message: result.error });
+			const user = await UserModel.getById(userId);
+
+			return res.status(200).json({ data: user });
+		} catch (e) {
+			next(e);
 		}
 	}
 
-	static async deleteUser(req: Request, res: Response) {
-		const userId = req.session.userId ?? req.user?.id;
+	static async deleteUser(req: Request, res: Response, next: NextFunction) {
+		try {
+			const userId = req.session.userId ?? req.user?.id;
 
-		if (!userId) {
-			res.status(401).send({ message: 'no active session.' });
-			return;
-		}
+			if (!userId) {
+				throw new RequestError('no active session.');
+			}
 
-		const result = await AuthService.deleteUser(userId);
-		if (result.status) {
-			res.status(200).send({ message: 'success.' });
-		} else {
-			res.status(400).send({ message: result.error });
+			await UserModel.delete(userId);
+		} catch (e) {
+			next(e);
 		}
 	}
 }

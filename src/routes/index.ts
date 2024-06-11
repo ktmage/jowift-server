0b0b9158ef1fcbfd7aca1e3888cbdae2c7a00ca8
ktmage <yasashisainternet@gmail.com>
2024-06-12 00:06:07 +0900

    [fix] : importを修正

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import { isAuthenticated } from '@/middleware';
-import AuthRouter from './AuthRouter';
-import NoteRouter from './NoteRouter';
-import TagRouter from './TagRouter';
+import authRouter from './authRouter';
+import noteRouter from './noteRouter';
+import tagRouter from './tagRouter';
 
 const router = express.Router();
 
 // 認証用のルーティング
-router.use('/auth', AuthRouter);
+router.use('/auth', authRouter);
 
 // 認証が必要なルーティング
 router.use(isAuthenticated);
 
-router.use('/note', NoteRouter);
-router.use('/tag', TagRouter);
+router.use('/note', noteRouter);
+router.use('/tag', tagRouter);
 
 export default router;
